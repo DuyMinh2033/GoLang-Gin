525f605b1bd799d8a51cceda9e5a7fa88db223a5
Duy Minh <tdminh334546@gmail.com>
2025-04-02 17:02:41 +0700

    comment

--- test/main.go
+++ test/main.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	intList := []int{1, 3, 5, 9, 4, 2, 0}
-	sort.Sort(sort.Reverse(sort.IntSlice(intList)))
+	sort.Sort(sort.Reverse(sort.IntSlice(intList)))//asd
 	fmt.Println(intList)
 }
